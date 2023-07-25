e2415b6c0e45e83e471e1d788d8e18afdad95a6d
Yasmiley <tshicilayamkela@gmail.com>
2023-07-24 18:47:41 -0700

    Write a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: string to be printed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n' ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
